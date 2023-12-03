c57c2b2e5ed30bba91003ded20c606f7abebc6be
CamoGekko <142858559+CamoGekko@users.noreply.github.com>
2023-12-03 15:25:31 -0800

    Alright First Commit :thumbsup:

---
+++ src/meme/content/MemeItems.java
@@ -0,0 +1,24 @@
+package src.meme.content;
+
+import arc.graphics.*;
+import mindustry.type.*;
+
+public class MemeItems{
+    public static Item pyratite, phaseFabric;
+
+    public static void load(){
+
+    pyratite = new Item("pyratite", Color.valueOf("ffaa5f")){{
+        flammability = 20f;
+        explosiveness = 1f;
+        buildable = false;
+    }};
+    phaseFabric = new Item("phase-fabric", Color.valueOf("f4ba6e")){{
+        radioactivity = "none";
+        cost = 1.3f
+        healthScaling = 0.25f
+    }};
+}
+
+
+ 
---
+++ src/meme/content/MemeLiquids.java
@@ -0,0 +1,23 @@
+package mindustry.content;
+
+import arc.graphics.*;
+import mindustry.type.*;
+
+public class MemeLiquids{
+    public static Liquid cryofluid;
+
+    public static void load(){
+
+    cryofluid = new Liquid("cryofluid", Color.valueOf("6ecdec")){{
+        heatCapacity = 1f;
+        temperature = 0.05f;
+        effect = StatusEffects.freezing;
+        lightColor = Color.valueOf("0097f5").a(0.2f);
+        boilPoint = 0.55f;
+        gasColor = Color.valueOf("c1e8f5");
+        }
+    };
+}
+
+
+ 
